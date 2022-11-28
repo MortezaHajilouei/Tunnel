e866430b6cbe9b0b0d2323c1f6c0f616077a99fd
morteza <mortezahajilouei@gmail.com>
2022-11-28 18:54:52 +0330

    add files

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const express = require('express');
 const net = require('net');
 
 const app = express();
-const SocketHandler = require('./SocketHandler') 
+const SocketHandler = require('./SocketHandler')
 const PORT = process.env.PORT || 8080;
- 
+
 const server = net.createServer(socket => {
+  console.log(socket)
   new SocketHandler(socket, {}).handle();
 });
 
-server.listen(PORT, function() {
+server.listen(PORT, function () {
   console.log(`Listening on port ${PORT}`);
 });
