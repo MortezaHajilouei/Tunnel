444104a67abc43b3773224f6c201bcb398041fe4
morteza <mortezahajilouei@gmail.com>
2022-11-28 19:06:05 +0330

    update

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Tunnel
+
+
+git commit -am "update"
+git push
+git push heroku main
+heroku logs --tail
--- index.js
+++ index.js
@@ -1,19 +1,41 @@
-const express = require('express');
-const net = require('net');
+// const express = require('express');
+// const net = require('net');
 
-const app = express();
-const SocketHandler = require('./SocketHandler')
+// const app = express();
+// const SocketHandler = require('./SocketHandler')
 
-const server = net.createServer(socket => {
-  console.log(socket)
-  new SocketHandler(socket, {}).handle();
+// const server = net.createServer(socket => {
+//   console.log(socket)
+//   new SocketHandler(socket, {}).handle();
+// });
+
+// server.listen(8080, function () {
+//   console.log(`Listening on port ${8080}`);
+// });
+
+
+// app.listen(8081,function () {
+//   console.log(`Listening on port ${8081}`);
+// })
+
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.get('/', function(req, res) {
+   res.send('index.html');
 });
 
-server.listen(8080, function () {
-  console.log(`Listening on port ${8080}`);
+//Whenever someone connects this gets executed
+io.on('connection', function(socket) {
+   console.log('A user connected');
+
+   //Whenever someone disconnects this piece of code executed
+   socket.on('disconnect', function () {
+      console.log('A user disconnected');
+   });
 });
 
-
-app.listen(8081,function () {
-  console.log(`Listening on port ${8081}`);
-})
+http.listen(3000, function() {
+   console.log('listening on *:3000');
+});
